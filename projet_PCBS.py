baada11a30811ca341710ed3d6bfd89752b70be1
OndineS <45490387+OndineS@users.noreply.github.com>
2019-01-24 03:24:54 +0100

    Add files via upload

--- projet_PCBS.py
+++ projet_PCBS.py
@@ -3,19 +3,20 @@ from expyriment import design, control, stimuli, io, misc
 import random
 
 ############### INITIALISATION DE L'EXPERIENCE #################
+
 exp = design.Experiment("Projet PCBS: information crossing hemispheres")
 control.initialize(exp)
-user_device = exp.keyboard # Créer la classe du périphérique de saisie
+screen_size = exp.screen.surface.get_size()
+user_device = exp.keyboard
 
 ##################### INITIALISATION DES PARAMETRES #####################
-# (Permet de modifier facilement le design expérimental si nécessaire)
 
 key_m = 59
 key_q = 97
 response_keys = [key_m, key_q]
 
-n_trial = 5 # Nombre de trials par bloc
-fixcross = stimuli.FixCross(size=(20, 20), line_width = 3) #Croix de fixation
+nb_trial = 5
+fixcross = stimuli.FixCross(size=(20, 20), line_width = 3)
 fixcross.preload()
 Blankscreen = stimuli.BlankScreen()
 
@@ -26,26 +27,30 @@ left_square = stimuli.Rectangle(square_size, position=[-square_dist, 0])
 left_square.preload()
 right_square = stimuli.Rectangle(square_size, position=[square_dist, 0])
 right_square.preload()
+square_displaytime = 100
 
-# Paramètre de réponses: appuyer sur "M" si stimulus à droite / "Q" si stimulus à gauche
+# Définition des stimuli et paramètres de réponse utilisateur
 trial_left = ["left", key_q, left_square, 0]
 trial_right = ["right", key_m, right_square, 0]
 
-square_wait = 100 # Durée d'affichage du stimulus
-wait_duration = 1000 # Durée d'attente post-stimulus pour saisie utilisateur
+wait_duration = 1000
 
 ##################### INSTRUCTIONS #####################
 
-instructions = """Dans l'expérience suivante, un carré gris va apparaître dans la partie droite ou gauche de votre champ visuel,
-de part et d'autre d'une croix centrale.
-Vous devez fixer la croix pendant toute la durée de l'essai.
-Appuyez sur la touche "M" si le carré apparaît à droite de la croix, et sur "Q" s'il apparaît à gauche.
+instructions = """Dans l'expérience suivante, un carré gris va apparaître dans la partie droite ou gauche de votre champ visuel, \n
+de part ou d'autre d'une croix centrale. \n
+Vous devez fixer la croix pendant toute la durée de l'essai.\n
+Appuyez sur la touche \"M\" si le carré apparaît à droite de la croix, et sur \"Q\" s'il apparaît à gauche.\n
 
 Appuyez sur n'importe quelle touche pour continuer."""
 
-instruction1 = "Veuillez positionner votre index droit sur la touche \"m\" et votre index gauche sur la touche \"q\" puis taper sur n'importe quelle touche pour commencer"
-instruction2 = "Veuillez positionner votre index gauche sur la touche \"m\" et votre index droit sur la touche \"q\", votre bras droit étant placé au-dessus de votre bras gauche, puis taper sur n'importe quelle touche pour commencer"
-instruction3 = "Veuillez positionner votre index gauche sur la touche \"m\" et votre index droit sur la touche \"q\", votre bras gauche étant placé au-dessus de votre bras droit puis taper sur n'importe quelle touche pour commencer"
+instruction1 = "Veuillez positionner votre index droit sur la touche \"M\" et votre index gauche sur la touche \"Q\" \n Puis appuyez sur n'importe quelle touche pour commencer"
+instruction2 = "Veuillez positionner votre index gauche sur la touche \"M\" et votre index droit sur la touche \"Q\",
+Votre bras droit étant placé au-dessus de votre bras gauche, \n
+Puis appuyez sur n'importe quelle touche pour commencer"
+instruction3 = "Veuillez positionner votre index gauche sur la touche \"M\" et votre index droit sur la touche \"Q\", \n
+Votre bras gauche étant placé au-dessus de votre bras droit, \n
+Puis appuyez sur n'importe quelle touche pour commencer"
 instructions_total = [instruction1, instruction2, instruction3]
 
 ##################### CREATION DES BLOCS D'EXPERIMENTATION #####################
@@ -54,11 +59,9 @@ for nblock in ["Bloc 1", "Bloc 2", "Bloc 3"]:
 	b = design.Block()
 	b.set_factor("Numéro du bloc", nblock)
 
-# Définition du nombre de tirages par côté (au moins 1 de chaque côté)
-	trial_left[3] = int(n_trial/2)
-	trial_right[3] = n_trial - trial_left[3]
+	trial_left[3] = int(nb_trial/2)
+	trial_right[3] = nb_trial - trial_left[3]
 
-# Création des stimuli carrés "gauche" et "droite"
 	for where in [trial_left, trial_right]:
 			t = design.Trial()
 			t.set_factor("Position", where[0])
@@ -68,22 +71,21 @@ for nblock in ["Bloc 1", "Bloc 2", "Bloc 3"]:
 			b.add_trial(t, copies=where[3])
 	b.shuffle_trials()
 
-	exp.add_block(b) # Ajout du bloc à l'expérience
+	exp.add_block(b)
 
-# Variables d’expérimentation à conserver: numéro de bloc, position du stimulus, saisie clavier attendue,saisie de l’utilisateur, temps de réaction
-exp.data_variable_names = ["Bloc", "Position", "Expected", "Button", "RT"]
+exp.data_variable_names = ["Bloc", "Position", "Expected", "Button", "RT"] # Variables à enregistrer à chaque essai
 
-##################### DEBUT DE L'EXPERIENCE #####################
+##################### PRESENTATION DE L'EXPERIENCE #####################
 
 control.start()
 
-stimuli.TextScreen("Instructions", instructions).present() # instructions générales
+stimuli.TextScreen("Instructions", instructions).present()
 user_device.wait()
-# Enchaînement des trois blocs
-j = 0
+
+block_index = 0
 for block in exp.blocks:
-	stimuli.TextScreen("Instructions", instructions_total[j] ).present() # instructions spécifiques
-	j += 1
+	stimuli.TextScreen("Instructions", instructions_total[block_index] ).present()
+	block_index += 1
 	user_device.wait()
 
 	for trial in block.trials:
@@ -91,14 +93,12 @@ for block in exp.blocks:
 		fixcross.present()
 		exp.clock.wait(show_time)
 
-		 #affichage du stimulus
 		trial.stimuli[0].present()
-		exp.clock.wait(square_wait)
+		exp.clock.wait(square_displaytime)
 		Blankscreen.present()
 
 		button, rt = user_device.wait(keys = response_keys, duration = wait_duration)
 
-# Enregistrement des données obtenues:
-		exp.data.add([block.get_factor("Numéro du bloc"), trial.get_factor("Position"), trial.get_factor("Expected"), button, rt])
+		exp.data.add([block.get_factor("Numéro du bloc"), trial.get_factor("Position"), trial.get_factor("Expected"), button, rt]) # Exportation des données obtenues
 
 control.end()
